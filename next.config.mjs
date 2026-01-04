3e154c6c0e8946a9d738b1742c8bea0c5db698a4
Emogy-reunion <mv7786986@gmail.com>
2026-01-04 13:58:46 +0300

    refactor: configure Next.js rewrites to proxy /api requests to Flask, enabling /api/send_image for <Image>

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  // Proxy /api requests to your Flask backend
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',                // any request starting with /api
+        destination: 'http://127.0.0.1:5000/:path*', // goes to Flask
+      },
+    ];
+  },
+
 };
 
 export default nextConfig;
