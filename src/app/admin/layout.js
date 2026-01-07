fe44b9f1cb7e328aaa383b62d5273aa550c26c26
Emogy-reunion <mv7786986@gmail.com>
2026-01-07 14:07:34 +0300

    add: add logic that ensures the user is authenticated before they access a page

---
+++ src/app/admin/layout.js
@@ -0,0 +1,50 @@
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+export const dynamic = 'force-dynamic';
+
+
+export default async function AdminLayout({ children }) {
+
+	try {
+		// forward cookies to backend
+		const cookie = cookies().toString();
+
+		let response = await fetch(`${BACKEND_URL}/api/is_logged_in`, {
+			method: 'GET',
+			headers: { cookie }
+			cache: 'no-store',
+		});
+
+
+		if (!response.ok) {
+			
+			const refreshRes = await fetch(`${BACKEND_URL}/api/refresh_token`, {
+				method: 'POST', 
+				headers: { cookie },
+				cache: 'no-store',
+			});
+
+			if (!refreshRes.ok) {
+				redirect('/guest/login');
+			}
+
+
+			response = await fetch(`${BACKEND_URL}/api/is_logged_in`, {
+				method: 'GET',
+                                headers: { cookie }
+                                cache: 'no-store',
+                	});
+		}
+
+		const data = await response.json();
+
+		if (data.role != 'admin') {
+			redirect('/guest/login');
+		}
+
+		return children;
+	} catch(error) {
+		redirect('/guest/login');
+	}
+}
