7242f874f8b528bce4754f27a3d5d4cd6aaee1b7
Emogy-reunion <mv7786986@gmail.com>
2026-01-22 14:22:43 +0300

    refactor: redesign the app to use a single page to display all guest products

---
+++ src/app/guest/products/page.js
@@ -0,0 +1,37 @@
+import { cookies } from 'next/headers';
+import GuestNetworkComponent from '@/app/guest/products/network/guestNetworkComponent';
+
+
+const BACKEND_URL = process.env.BACKEND_URL
+
+
+const  GuestProductsPage = async ({ searchParams }) => {
+	
+
+	const group = (await searchParams).group || "";
+	const category = (await searchParams).category || "";
+
+        try {
+
+		let url = `${BACKEND_URL}/api/get_product_previews?`;
+        	if (group) url += `group=${group}&`;
+        	if (category) url += `category=${category}&`;
+
+                const response = await fetch(url, {
+                        method: 'GET',
+                        cache: 'no-store',
+                });
+
+                if (!response.ok) {
+                        throw new Error('Fail to fetch products. Refresh page and try again!');
+                }
+
+                const data = await response.json();
+
+                return <GuestNetworkComponent data={data} group={group} category={category} />;
+        } catch(error) {
+        }
+};
+
+
+export default GuestProductsPage;
